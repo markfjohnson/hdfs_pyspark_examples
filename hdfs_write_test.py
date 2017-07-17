6265146b67a3a19a78e8e7a4316165c8379acfa0
markjohnson <markfjohnson@gmail.com>
2017-07-17 17:59:25 -0400

    Removed the faker for this test progrAm

---
+++ hdfs_write_test.py
@@ -0,0 +1,73 @@
+rom pyspark import *
+from pyspark import Row
+
+
+
+from random import randrange,randint,choice
+import uuid
+import datetime
+import os
+
+
+
+
+print (os.getlogin())
+print (os.getenv("SPARK_HOME","spark home"))
+
+tableName = "CUSTOMER_INFO10"
+
+#----------------------------
+sc = SparkContext(appName = "WriteRDD_10m")
+sc.addPyFiles("simple_hdfs_writer-0.1-py2.7.egg")
+
+
+#----------------------------
+
+rowDef = Row("transGroup","CustomerId","companyGroup","CompanyName","Name","EMAIL","Address","CustomerSince","AnnualSales","LastOrderDate")
+
+
+
+
+#----------------------------
+def random_date(start, end):
+    secRange = randrange(0, (end - start).total_seconds(),1)
+    d = start + datetime.timedelta(secRange)
+    return(unicode(d))
+
+
+
+def buildCustomerRDD(i) :
+    d1 = datetime.date(randrange(2008,2016,1),randrange(1,12,1),1)
+    d2 = datetime.date(2016,12,31)
+    row = rowDef(i, str(uuid.uuid4()), choice(["GroupA","GroupB","GroupC"]),"ACME Widget", "John Doe", "abc@abc.com", "ABC",randrange(1990,2016,1), randrange(1000,10000,1000),d1)
+    return(row);
+
+#---------------- Begin Main Processing
+a = sc.parallelize(range(1,100))
+rddRows = a.map(lambda i: buildCustomerRDD(i) )
+print(rddRows.toDebugString())
+print("----------- Using the predefined schema")
+
+print("----------- Using the inferred schema")
+
+
+#df.printSchema()
+print("----------- \n\n\n\n")
+
+
+#hc.sql("CREATE TABLE CUSTOMER_INFO3 (transGroup int,customerId STRING, CompanyName String,contactName String, EMAIL string,Address string,CustomerSince INT, AnnualSales FLOAT,LastOrderDate DATE);")
+
+#df.show()
+print("----------------------")
+
+#print (df.count())
+startTime = datetime.datetime.now()
+#a = df.collect()
+endTime = datetime.datetime.now()
+print("Elapsed Time = {0}".format(endTime-startTime))
+df.write.format("csv").mode("overwrite").saveAsTable(tableName)
+#def toCSVLine(data):
+#  return ','.join(str(d) for d in data)
+#
+#lines = df.map(toCSVLine)
+#lines.saveAsTextFile('CustomerInfo')
